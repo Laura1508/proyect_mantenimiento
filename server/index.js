695187711532af03c742148b3f7cdf63da8f5af4
Emmanuel Vanegas Carmona <Ximandiv@gmail.com>
2026-05-22 20:26:21 -0500

    Refactors get endpoint to use filter and pagination by cursor

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const db = require('./db');
+const { parse } = require('dotenv');
 
 const app = express();
 
@@ -10,11 +11,64 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/docentes', (req, res) => {
-    const sql = 'SELECT * FROM docentes';
+    const {
+        cursor_id = '0', limite = '10', 
+        nombre, correo, area_academica, dedicacion, titulo, anios_experiencia} = req.query;
 
-    db.query(sql, (err, results) =>{
+    const cursorIdNum = parseInt(cursor_id, 10);
+    const limiteNum = parseInt(limite, 10);
+
+    if (Number.isNaN(cursorIdNum) || cursorIdNum < 0) {
+        return res.status(400).json({error: 'cursor_id inválido'});
+    }
+    if (Number.isNaN(limiteNum) || limiteNum <= 0) {
+        return res.status(400).json({error: 'limite inválido'});
+    }
+
+    let aniosFilterNum;
+    if (anios_experiencia) {
+        aniosFilterNum = parseInt(anios_experiencia, 10);
+        if (Number.isNaN(aniosFilterNum) || aniosFilterNum < 0) {
+            return res.status(400).json({error: 'anios_experiencia inválido'});
+        }
+    }
+
+    let sqlQuery = 'SELECT * FROM docentes ' +
+            'WHERE id > ? ';
+    const queryParams = [cursorIdNum];
+
+    if (nombre) {
+        sqlQuery += 'AND nombre LIKE ? ';
+        queryParams.push(`%${nombre}%`);
+    }
+    if (correo) {
+        sqlQuery += 'AND correo LIKE ? ';
+        queryParams.push(`%${correo}%`);
+    }
+    if (area_academica) {
+        sqlQuery += 'AND area_academica LIKE ? ';
+        queryParams.push(`%${area_academica}%`);
+    }
+    if (dedicacion) {
+        sqlQuery += 'AND dedicacion LIKE ? ';
+        queryParams.push(`%${dedicacion}%`);
+    }
+    if (titulo) {
+        sqlQuery += 'AND titulo LIKE ? ';
+        queryParams.push(`%${titulo}%`);
+    }
+    if (anios_experiencia) {
+        sqlQuery += 'AND anios_experiencia >= ? ';
+        queryParams.push(aniosFilterNum);
+    }
+
+    sqlQuery += 'ORDER BY id ASC ' +
+            'LIMIT ?';
+
+    db.query(sqlQuery, [...queryParams, limiteNum], (err, results) =>{
         if (err) {
-            return res.status(500).json({error: 'error al obtener los docentes'});
+            console.error('Error al obtener los docentes:', err);
+            return res.status(500).json({error: 'Error al obtener los docentes'});
         }
         res.json(results);
     });
